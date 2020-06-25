59e8f7c048c3ed594d84e721d17ba315289406f4
dsridhar45 <sridhard@dextara.com>
2020-06-25 14:20:40 +0530

    componentscan

--- src/main/java/com/qualifyde/controller/QualifydeApplication.java
+++ src/main/java/com/qualifyde/controller/QualifydeApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
-
+@ComponentScan({"com.qualifyde.dao"})
 @SpringBootApplication
 public class QualifydeApplication  extends SpringBootServletInitializer {
     
